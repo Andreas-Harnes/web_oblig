f77c26704dd94afcd1eed6e4b161c40bba1b1cc1
Mosti <mizgog123@hotmail.com>
2016-10-15 16:53:36 +0200

    La til funksjoner til hver knapp

--- javascript/main.js
+++ javascript/main.js
@@ -7,10 +7,70 @@ var handlekurv = [];
 
 function oppstart()
 {
-	document.getElementsByClassName("btnKjop").onclick = kjopet;
+	document.getElementById("kjop1").onclick = kjop1;
+	document.getElementById("kjop1").onclick = kjop2;
+	document.getElementById("kjop1").onclick = kjop3;
+	document.getElementById("kjop1").onclick = kjop4;
+	document.getElementById("kjop1").onclick = kjop5;
+	document.getElementById("kjop1").onclick = kjop6;
+	document.getElementById("kjop1").onclick = kjop7;
+	document.getElementById("kjop1").onclick = kjop8;
+	document.getElementById("kjop1").onclick = kjop9;
+	document.getElementById("kjop1").onclick = kjop10;
+	document.getElementById("kjop1").onclick = kjop11;
+	document.getElementById("kjop1").onclick = kjop12;
 }
 
-function kjopet()
+function kjop1()
+{
+	
+}
+
+function kjop2()
+{
+	console.log("funker");
+}
+
+function kjop3()
+{
+	console.log("funker");
+}
+
+function kjop4()
+{
+	console.log("funker");
+}
+
+function kjop5()
+{
+	console.log("funker");
+}
+
+function kjop6()
+{
+	console.log("funker");
+}
+function kjop7()
+{
+	console.log("funker");
+}
+function kjop8()
+{
+	console.log("funker");
+}
+function kjop9()
+{
+	console.log("funker");
+}
+function kjop10()
+{
+	console.log("funker");
+}
+function kjop11()
+{
+	console.log("funker");
+}
+function kjop12()
 {
 	console.log("funker");
 }

134f71e2035a5c2e01c9ebfb74e7aade03f017fa
Mosti <mizgog123@hotmail.com>
2016-10-15 16:29:45 +0200

    Lagt til javascript for knapper

---
+++ javascript/main.js
@@ -0,0 +1,16 @@
+// Her kommer handlekurv funksjonaliteten til nettsiden
+
+// Variabler
+
+windows.onload = oppstart;
+var handlekurv = [];
+
+function oppstart()
+{
+	document.getElementsByClassName("btnKjop").onclick = kjopet;
+}
+
+function kjopet()
+{
+	
+}
